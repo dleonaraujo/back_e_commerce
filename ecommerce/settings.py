e3189e858b6d331fe56e6f5fcbfffa94e04df3d0
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2026-04-22 09:59:17 -0500

    Ajustando settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -131,3 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# ESTA ES LA LÍNEA QUE TE FALTA:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Esto ayuda a WhiteNoise a servir los archivos de forma eficiente
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
